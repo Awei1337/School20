f064a68742e84f43ec96b02e2254497fbf3b2a64
Awei1337 <weidordard@gmail.com>
2025-03-04 20:03:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-function toggleDescription() {
-    const description = document.getElementById('full-description');
-    const showMoreButton = document.querySelector('.show-more');
 
-    if (description.style.display === "none" || description.style.display === "") {
-        description.style.display = "block";
-        showMoreButton.textContent = "Скрыть описание";
-    } else {
-        description.style.display = "none";
-        showMoreButton.textContent = "Читать полное описание";
-    }
-}
+document.querySelector('.full-description-toggle').addEventListener('click', function() {
+    const description = document.querySelector('.full-description');
+    description.style.display = description.style.display === 'block' ? 'none' : 'block';
+});
