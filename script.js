d2d5bd961f30ba4b546214a2548b1f9fda5100f5
Awei1337 <weidordard@gmail.com>
2025-03-11 00:50:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+// Слайдшоу
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 7000); // Изменить фото каждые 7 секунд
+}
+
+// Открытие/закрытие текста
 document.querySelector('.full-description-toggle').addEventListener('click', function() {
-    const description = document.querySelector('.full-description');
-    description.style.display = description.style.display === 'block' ? 'none' : 'block';
+    let description = document.querySelector('.full-description');
+    if (description.style.display === "none" || description.style.display === "") {
+        description.style.display = "block";
+    } else {
+        description.style.display = "none";
+    }
 });
